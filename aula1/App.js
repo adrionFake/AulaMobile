50adc98bcd70b0e0ccc2871dbb27e7616015494d
ADRION ARAUJO MARTINS <adrion.martins@ACAD.UNIVATES>
2025-08-06 20:41:19 -0300

    A

---
+++ aula1/App.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>SequestroEncomendado.com</Text>
+      
+      <Text>Abacaxi de morango</Text>
+    </View>
+  );
+}
---
+++ aula2/aula/App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Quero motor da Yanagi ;-;</Text>
+      
+    </View>
+  );
+}
